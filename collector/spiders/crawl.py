0d792b95374e22ecfba9300cfaaf21e0fa9b3745
mystic <215104920@qq.com>
2018-12-27 21:21:58 +0800

    [scrapy] in action

--- collector/spiders/crawl.py
+++ collector/spiders/crawl.py
@@ -3,15 +3,22 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
 
-class CrawlSpider(CrawlSpider):
-    name = 'crawl'
-    allowed_domains = ['example.com']
-    start_urls = ['http://example.com/']
+class AdaCrawlSpider(CrawlSpider):
+    name = 'Ada'
+    allowed_domains = ['qq.com']
+    start_urls = ['https://y.qq.com/']
+
+    headers = {
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "Referer": "https://login.xiami.com/member/login?spm=a1z1s.6843761.226669498.1.2iL1jx"
+    }
 
     rules = (
         Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
     )
 
+    # noinspection PyMethodMayBeStatic
     def parse_item(self, response):
         i = {}
         # i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
